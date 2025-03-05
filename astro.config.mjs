28537bacca34602ccef676b7f2c0d2983b65914b
andrea <ag53755@fer.hr>
2025-03-05 19:48:16 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+//export default defineConfig({});
+
+// @ts-check
+import tailwindcss from "@tailwindcss/vite";
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  
+});
